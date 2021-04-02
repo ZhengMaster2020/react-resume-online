dcc2aa4869c50ad1719965638d15966edea4a6d5
lizheng <lizheng@668dingtalk.com>
2021-04-02 23:49:02 +0800

    feat: add resume

--- pages/components/ResumeDepot/components/ResumeAdd.js
+++ pages/components/ResumeDepot/components/ResumeAdd.js
@@ -10,10 +10,34 @@ import {
   recommedItemBtn,
 } from '../../../../styles/ResumeAdd.module.css'
 
-const tmplDataArr = [
+const tmplDataSource = [
   {
     src: 'https://files.wondercv.com/cvsvuetpl-college.png',
-    title: '零经验实习',
+    title: '零经验实习1',
+    des: '学生找实习或全职工作，有一定实习经历的同学',
+    isShowBtn: true,
+    zhBtnType: 'default',
+    enBtnType: 'default',
+  },
+  {
+    src: 'https://files.wondercv.com/cvsvuetpl-college.png',
+    title: '零经验实习2',
+    des: '学生找实习或全职工作，有一定实习经历的同学',
+    isShowBtn: true,
+    zhBtnType: 'default',
+    enBtnType: 'default',
+  },
+  {
+    src: 'https://files.wondercv.com/cvsvuetpl-college.png',
+    title: '零经验实习3',
+    des: '学生找实习或全职工作，有一定实习经历的同学',
+    isShowBtn: true,
+    zhBtnType: 'default',
+    enBtnType: 'default',
+  },
+  {
+    src: 'https://files.wondercv.com/cvsvuetpl-college.png',
+    title: '零经验实习4',
     des: '学生找实习或全职工作，有一定实习经历的同学',
     isShowBtn: true,
     zhBtnType: 'default',
@@ -21,196 +45,82 @@ const tmplDataArr = [
   },
 ]
 
-const CardItem = () => {
-  tmplDataArr.map(({ src, title, des, isShowBtn, zhBtnType, enBtnType }) => {
-    return (
-      <div
-        className={recommedItem}
-        onMouseOver={() => handleShowBtn(isShowBtn)}
-        onMouseOut={() => handleHiddenBtn(isShowBtn)}
-      >
-        <div
-          id="zero"
-          className={recommedItem}
-          style={{ display: isShowBtn ? 'flex' : 'none' }}
-        >
-          <img src={src} className={recommedItemImg} />
-          <div className={recommedItemDesc}>
-            {' '}
-            s<p className={recommedItemTitle}>{title}</p>
-            <p className={recommedItemText}>{des}</p>
-          </div>
-        </div>
-
-        <div
-          className={recommedItemBtn}
-          style={{ display: isShowBtn ? 'none' : 'grid' }}
-        >
-          <Button
-            danger
-            id="zh"
-            type={isMoveOver ? 'primary' : 'default'}
-            onMouseOver={() => handleShowBtn('zero')}
-            onMouseOut={() => handleHiddenBtn('zero')}
-          >
-            中文模板
-          </Button>
-          <Button
-            danger
-            id="en"
-            type={false ? 'primary' : 'default'}
-            onMouseOver={() => handleShowBtn('zero')}
-            onMouseOut={() => handleHiddenBtn('zero')}
-          >
-            英文模板
-          </Button>
-        </div>
-      </div>
-    )
-  })
-}
-
 // 新建简历
 const ReusmeAdd = () => {
-  let [isMoveOver, setIsMoveOver] = useState(true)
-  const handleShowBtn = (curItem) => {
-    console.log('0000', curItem)
-    setIsMoveOver(false)
+  let [tmplDataArr, setIsMoveOver] = useState(tmplDataSource)
+
+  const handleShowBtn = (idx, isShowBtn) => {
+    tmplDataSource[idx].isShowBtn = isShowBtn
+    setIsMoveOver(tmplDataSource.slice())
   }
-  const handleHiddenBtn = (curItem) => {
-    console.log('1111', curItem)
-    setIsMoveOver(true)
+
+  const handleBtnBgColor = (options) => {
+    const { idx, zhBtnType, enBtnType } = options
+    tmplDataSource[idx].zhBtnType = zhBtnType
+    tmplDataSource[idx].enBtnType = enBtnType
+    setIsMoveOver(tmplDataSource.slice())
+  }
+
+  // card模板组件
+  const CardItem = () => {
+    return tmplDataArr.map(
+      ({ src, title, des, isShowBtn, zhBtnType, enBtnType }, idx) => {
+        return (
+          <div
+            key={title}
+            className={recommedItem}
+            onMouseEnter={() => handleShowBtn(idx, false)}
+            onMouseLeave={() => handleShowBtn(idx, true)}
+          >
+            <div
+              className={recommedItem}
+              style={{ display: isShowBtn ? 'flex' : 'none' }}
+            >
+              <img src={src} className={recommedItemImg} />
+              <div className={recommedItemDesc}>
+                <p className={recommedItemTitle}>{title}</p>
+                <p className={recommedItemText}>{des}</p>
+              </div>
+            </div>
+
+            <div
+              className={recommedItemBtn}
+              style={{ display: isShowBtn ? 'none' : 'grid' }}
+            >
+              <Button
+                danger
+                type={zhBtnType}
+                onMouseEnter={() =>
+                  handleBtnBgColor({ idx, enBtnType, zhBtnType: 'primary' })
+                }
+                onMouseLeave={() =>
+                  handleBtnBgColor({ idx, enBtnType, zhBtnType: 'default' })
+                }
+              >
+                中文模板
+              </Button>
+              <Button
+                danger
+                type={enBtnType}
+                onMouseEnter={() =>
+                  handleBtnBgColor({ idx, zhBtnType, enBtnType: 'primary' })
+                }
+                onMouseLeave={() =>
+                  handleBtnBgColor({ idx, zhBtnType, enBtnType: 'default' })
+                }
+              >
+                英文模板
+              </Button>
+            </div>
+          </div>
+        )
+      }
+    )
   }
 
   return (
     <div className={recommedWrap}>
-      <div
-        className={recommedItem}
-        onMouseOver={() => handleShowBtn('zero')}
-        onMouseOut={() => handleHiddenBtn('zero')}
-      >
-        <div
-          id="zero"
-          className={recommedItem}
-          style={{ display: isMoveOver ? 'flex' : 'none' }}
-        >
-          <img src="" className={recommedItemImg} />
-          <div className={recommedItemDesc}>
-            <p className={recommedItemTitle}>零经验实习</p>
-            <p className={recommedItemText}>
-              学生找实习或全职工作，有一定实习经历的同学
-            </p>
-          </div>
-        </div>
-
-        <div
-          className={recommedItemBtn}
-          style={{ display: isMoveOver ? 'none' : 'grid' }}
-        >
-          <Button
-            danger
-            id="zh"
-            type={isMoveOver ? 'primary' : 'default'}
-            onMouseOver={() => handleShowBtn('zero')}
-            onMouseOut={() => handleHiddenBtn('zero')}
-          >
-            中文模板
-          </Button>
-          <Button
-            danger
-            id="en"
-            type={false ? 'primary' : 'default'}
-            onMouseOver={() => handleShowBtn('zero')}
-            onMouseOut={() => handleHiddenBtn('zero')}
-          >
-            英文模板
-          </Button>
-        </div>
-      </div>
-
-      <div className={recommedItem}>
-        <img
-          src="https://files.wondercv.com/cvsvuetpl-college.png"
-          className={recommedItemImg}
-        />
-        <div className={recommedItemDesc}>
-          <p className={recommedItemText}>
-            学生找实习或全职工作，有一定实习经历的同学
-          </p>
-        </div>
-      </div>
-
-      <div className={recommedItem}>
-        <img
-          src="https://files.wondercv.com/cvsvuetpl-college.png"
-          className={recommedItemImg}
-        />
-        <div className={recommedItemDesc}>
-          <p className={recommedItemText}>
-            学生找实习或全职工作，有一定实习经历的同学
-          </p>
-        </div>
-      </div>
-
-      <div className={recommedItem}>
-        <img
-          src="https://files.wondercv.com/cvsvuetpl-college.png"
-          className={recommedItemImg}
-        />
-        <div className={recommedItemDesc}>
-          <p className={recommedItemText}>
-            学生找实习或全职工作，有一定实习经历的同学
-          </p>
-        </div>
-      </div>
-
-      <div className={recommedItem}>
-        <img
-          src="https://files.wondercv.com/cvsvuetpl-college.png"
-          className={recommedItemImg}
-        />
-        <div className={recommedItemDesc}>
-          <p className={recommedItemText}>
-            学生找实习或全职工作，有一定实习经历的同学
-          </p>
-        </div>
-      </div>
-
-      <div className={recommedItem}>
-        <img
-          src="https://files.wondercv.com/cvsvuetpl-college.png"
-          className={recommedItemImg}
-        />
-        <div className={recommedItemDesc}>
-          <p className={recommedItemText}>
-            学生找实习或全职工作，有一定实习经历的同学
-          </p>
-        </div>
-      </div>
-
-      <div className={recommedItem}>
-        <img
-          src="https://files.wondercv.com/cvsvuetpl-college.png"
-          className={recommedItemImg}
-        />
-        <div className={recommedItemDesc}>
-          <p className={recommedItemText}>
-            学生找实习或全职工作，有一定实习经历的同学
-          </p>
-        </div>
-      </div>
-
-      <div className={recommedItem}>
-        <img
-          src="https://files.wondercv.com/cvsvuetpl-college.png"
-          className={recommedItemImg}
-        />
-        <div className={recommedItemDesc}>
-          <p className={recommedItemText}>
-            学生找实习或全职工作，有一定实习经历的同学
-          </p>
-        </div>
-      </div>
+      <CardItem />
     </div>
   )
 }
