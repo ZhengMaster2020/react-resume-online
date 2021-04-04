b526a824e47bdf07fae961c7a1e2c1ada5f194cc
zhengmaster <2105195274@qq.com>
2021-04-04 20:50:05 +0800

    feat: code foramt and add writing add resume template module

--- pages/components/ResumeDepot/components/ResumeAdd.js
+++ pages/components/ResumeDepot/components/ResumeAdd.js
@@ -1,98 +1,90 @@
-import { Button } from "antd";
-import { useState } from "react";
+import { Button } from 'antd';
+import { useState } from 'react';
 import {
-  recommedWrap,
-  recommedItem,
-  recommedItemDesc,
-  recommedItemImg,
-  recommedItemTitle,
-  recommedItemText,
-  recommedItemBtn,
-} from "../../../../styles/ResumeAdd.module.css";
-import { tmplDataSource } from "../../../../mocks/add-resume.js";
-import ResumeReviewTmpl from "./ResumeReviewTmpl";
+	recommedWrap,
+	recommedItem,
+	recommedItemDesc,
+	recommedItemImg,
+	recommedItemTitle,
+	recommedItemText,
+	recommedItemBtn,
+} from '../../../../styles/ResumeAdd.module.css';
+import { tmplDataSource } from '../../../../mocks/add-resume.js';
+import ResumeReviewTmpl from './ResumeReviewTmpl';
 
 // 新建简历组件
 const ReusmeAdd = () => {
-  let [tmplDataArr, setIsMoveOver] = useState(tmplDataSource);
+	let [tmplDataArr, setIsMoveOver] = useState(tmplDataSource);
+	const [isPreview, setPreview] = useState(false);
 
-  // 鼠标移入移出显示以及隐藏按钮模块
-  const handleShowBtn = (idx, isShowBtn) => {
-    tmplDataSource[idx].isShowBtn = isShowBtn;
-    setIsMoveOver(JSON.parse(JSON.stringify(tmplDataSource)));
-  };
+	const handleShowPreview = isShow => {
+		setPreview(isShow);
+	};
 
-  //    鼠标移入移出改变按钮类型模块
-  const handleBtnBgColor = (options) => {
-    const { idx, zhBtnType, enBtnType } = options;
-    tmplDataSource[idx].zhBtnType = zhBtnType;
-    tmplDataSource[idx].enBtnType = enBtnType;
-    setIsMoveOver(JSON.parse(JSON.stringify(tmplDataSource)));
-  };
+	const onVisibleChange = visible => {
+		console.log(visible, 'kpkkk');
+		setPreview(visible);
+	};
 
-  // card模板组件
-  const CardItem = () => {
-    return tmplDataArr.map(
-      ({ src, title, des, isShowBtn, zhBtnType, enBtnType }, idx) => {
-        return (
-          <div
-            key={title}
-            className={recommedItem}
-            onMouseEnter={() => handleShowBtn(idx, false)}
-            onMouseLeave={() => handleShowBtn(idx, true)}
-          >
-            <div
-              className={recommedItem}
-              style={{ display: isShowBtn ? "flex" : "none" }}
-            >
-              <img src={src} className={recommedItemImg} />
-              <div className={recommedItemDesc}>
-                <p className={recommedItemTitle}>{title}</p>
-                <p className={recommedItemText}>{des}</p>
-              </div>
-            </div>
+	// 鼠标移入移出显示以及隐藏按钮模块
+	const handleShowBtn = (idx, isShowBtn) => {
+		tmplDataSource[idx].isShowBtn = isShowBtn;
+		setIsMoveOver(JSON.parse(JSON.stringify(tmplDataSource)));
+	};
 
-            <div
-              className={recommedItemBtn}
-              style={{ display: isShowBtn ? "none" : "grid" }}
-            >
-              <Button
-                danger
-                type={zhBtnType}
-                onMouseEnter={() =>
-                  handleBtnBgColor({ idx, enBtnType, zhBtnType: "primary" })
-                }
-                onMouseLeave={() =>
-                  handleBtnBgColor({ idx, enBtnType, zhBtnType: "default" })
-                }
-              >
-                中文模板
-              </Button>
-              <Button
-                danger
-                type={enBtnType}
-                onMouseEnter={() =>
-                  handleBtnBgColor({ idx, zhBtnType, enBtnType: "primary" })
-                }
-                onMouseLeave={() =>
-                  handleBtnBgColor({ idx, zhBtnType, enBtnType: "default" })
-                }
-              >
-                英文模板
-              </Button>
-            </div>
-          </div>
-        );
-      }
-    );
-  };
+	//    鼠标移入移出改变按钮类型模块
+	const handleBtnBgColor = options => {
+		const { idx, zhBtnType, enBtnType } = options;
+		tmplDataSource[idx].zhBtnType = zhBtnType;
+		tmplDataSource[idx].enBtnType = enBtnType;
+		setIsMoveOver(JSON.parse(JSON.stringify(tmplDataSource)));
+	};
 
-  return (
-    <div className={recommedWrap}>
-      <CardItem />
-      <ResumeReviewTmpl />
-    </div>
-  );
+	// card模板组件
+	const CardItem = () => {
+		return tmplDataArr.map(({ src, title, des, isShowBtn, zhBtnType, enBtnType }, idx) => {
+			return (
+				<div
+					key={title}
+					className={recommedItem}
+					onMouseEnter={() => handleShowBtn(idx, false)}
+					onMouseLeave={() => handleShowBtn(idx, true)}>
+					<div className={recommedItem} style={{ display: isShowBtn ? 'flex' : 'none' }}>
+						<img src={src} className={recommedItemImg} />
+						<div className={recommedItemDesc}>
+							<p className={recommedItemTitle}>{title}</p>
+							<p className={recommedItemText}>{des}</p>
+						</div>
+					</div>
+
+					<div className={recommedItemBtn} style={{ display: isShowBtn ? 'none' : 'grid' }}>
+						<Button
+							danger
+							type={zhBtnType}
+							onClick={() => handleShowPreview(true)}
+							onMouseEnter={() => handleBtnBgColor({ idx, enBtnType, zhBtnType: 'primary' })}
+							onMouseLeave={() => handleBtnBgColor({ idx, enBtnType, zhBtnType: 'default' })}>
+							中文模板
+						</Button>
+						<Button
+							danger
+							type={enBtnType}
+							onMouseEnter={() => handleBtnBgColor({ idx, zhBtnType, enBtnType: 'primary' })}
+							onMouseLeave={() => handleBtnBgColor({ idx, zhBtnType, enBtnType: 'default' })}>
+							英文模板
+						</Button>
+					</div>
+				</div>
+			);
+		});
+	};
+
+	return (
+		<div className={recommedWrap}>
+			<CardItem />
+			{isPreview && <ResumeReviewTmpl visible={isPreview} onChange={onVisibleChange} />}
+		</div>
+	);
 };
 
 export default ReusmeAdd;
--- pages/components/ResumeDepot/components/ResumeReviewTmpl.js
+++ pages/components/ResumeDepot/components/ResumeReviewTmpl.js
@@ -1,22 +1,59 @@
-import { Image, Button, Space } from "antd";
+import { Modal, Tooltip } from 'antd';
+import { CloseCircleTwoTone, CheckCircleTwoTone } from '@ant-design/icons';
+import Image from 'next/image';
 
-const ResumeReviewTmpl = () => {
-  return (
-    <Space size={12}>
-      <Image
-        width={200}
-        src={`https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png`}
-        placeholder={
-          <Image
-            preview={false}
-            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png?x-oss-process=image/blur,r_50,s_50/quality,q_1/resize,m_mfit,h_200,w_200"
-            width={200}
-          />
-        }
-      />
-      <Button type="primary">Reload</Button>
-    </Space>
-  );
+const ResumeReviewTmpl = props => {
+	const { visible, onChange } = props;
+	const onVisibleChange = visible => onChange(visible);
+	const handleSelectTmpl = type => {
+		console.log('sssss', type);
+	};
+	const myLoader = ({ src, width, quality }) => {
+		return `https://files.wondercv.com/${src}?w=${width}&q=${quality || 75}`;
+	};
+
+	// 弹窗标题组件
+	const Title = () => (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'flex-end',
+				border: 'none',
+			}}>
+			<Tooltip title='使用该模板' color='#52c41a'>
+				<CheckCircleTwoTone
+					twoToneColor='#52c41a'
+					style={{ fontSize: '24px', cursor: 'pointer', marginRight: '20px' }}
+					onClick={() => handleSelectTmpl(false)}
+				/>
+			</Tooltip>
+			<CloseCircleTwoTone
+				twoToneColor='#ff7875'
+				style={{ fontSize: '24px', cursor: 'pointer' }}
+				onClick={() => onVisibleChange(false)}
+			/>
+		</div>
+	);
+
+	return (
+		<Modal
+			footer={null}
+			visible={visible}
+			closable={false}
+			bodyStyle={{ paddingTop: 0 }}
+			title={<Title />}
+			onCancel={() => onVisibleChange(false)}>
+			<div>
+				<Image
+					loader={myLoader}
+					src={`develop-temp-cn@2x.png`}
+					alt='Picture of the author'
+					width={500}
+					height={680}
+				/>
+			</div>
+		</Modal>
+	);
 };
 
 export default ResumeReviewTmpl;
