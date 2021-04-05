1d198d341fa0bc2c1a196e4a3a5ab3b0bd1fc8e2
lizheng <lizheng@668dingtalk.com>
2021-04-05 18:21:42 +0800

    feat: code format and add throttle

---
+++ utils/index.js
@@ -0,0 +1,23 @@
+// 函数节流
+export function throttle(func, wait = 200) {
+  // 利用闭包保存定时器和上次执行时间
+  let timer = null
+  let previous // 上次执行时间
+  return function () {
+    // 保存函数调用时的上下文和参数，传递给 fn
+    const context = this
+    const args = arguments
+    const now = +new Date()
+    if (previous && now < previous + wait) {
+      // 周期之中
+      clearTimeout(timer)
+      timer = setTimeout(function () {
+        previous = now
+        func.apply(context, args)
+      }, wait)
+    } else {
+      previous = now
+      func.apply(context, args)
+    }
+  }
+}
